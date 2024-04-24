5f95ecd66aa45e437ebeb3fc4c692a4c3dc1731d
Abdul Gaffar <csabdulgaffar@gmail.com>
2024-04-24 12:45:44 +0530

    first

---
+++ backend/data/products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    name: 'Frontend Web Development',
+    image: '/images/frontend.png',
+    description:
+      'Learn the skills and technologies required to build the user-facing components of a website or web application.',
+    category: 'Web Development',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+    brand: 'skillup',
+  },
+  {
+    name: 'Backend Web Development',
+    image: '/images/backend.jpg',
+    description:
+      'Master the server-side programming and database management skills needed to create dynamic and interactive web applications.',
+    category: 'Web Development',
+    price: 99.99,
+    countInStock: 7,
+    rating: 4.0,
+    brand: 'skillup',
+    numReviews: 8,
+  },
+  {
+    name: 'CSS Fundamentals',
+    image: '/images/css.jpg',
+    description:
+      'Learn the fundamentals of CSS (Cascading Style Sheets) to style and format the appearance of web pages.',
+    category: 'Web Development',
+    price: 49.99,
+    brand: 'skillup',
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'HTML Basics',
+    image: '/images/html.jpg',
+    description:
+      'Get started with HTML (Hypertext Markup Language) and learn how to structure the content of web pages.',
+    category: 'Web Development',
+    price: 29.99,
+    brand: 'skillup',
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'React.js: The Complete Guide',
+    image: '/images/react.png',
+    description:
+      'Learn React.js, a popular JavaScript library for building user interfaces, and create powerful and interactive web applications.',
+    category: 'Web Development',
+    price: 129.99,
+    brand: 'skillup',
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    name: 'MERN Stack Development',
+    image: '/images/mern.webp',
+    description:
+      'Master the MERN (MongoDB, Express.js, React.js, Node.js) stack and build full-stack web applications from scratch.',
+    category: 'Web Development',
+    price: 199.99,
+    countInStock: 0,
+    brand: 'skillup',
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'DevOps Fundamentals',
+    image: '/images/devops.png',
+    description:
+      'Learn the principles and practices of DevOps (Development and Operations) to improve software development and delivery processes.',
+    category: 'DevOps',
+    price: 79.99,
+    countInStock: 8,
+    brand: 'skillup',
+    rating: 4.5,
+    numReviews: 15,
+  },
+];
+
+export default products;
