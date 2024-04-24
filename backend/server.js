f92c1179ece64c05615f54ed4c18a015b4c8733f
Abdul Gaffar <csabdulgaffar@gmail.com>
2024-04-24 23:04:54 +0530

    'changed-tax'

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 
 
@@ -21,7 +20,6 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://skillhub-xyv4.onrender.com';
+export const BASE_URL = 'https://skillhub-xyv4.onrender.com/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
